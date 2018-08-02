306946fe822650a7ef389745a12f818f7760178d
John Lee <jhl22@outlook.com>
2018-08-02 01:26:16 -0700

    Add Client To Firestore

--- src/store.js
+++ src/store.js
@@ -25,7 +25,10 @@ const rrfConfig = {
 firebase.initializeApp(firebaseConfig);
 
 // Init firestore
-// const firestore = firebase.firestore();
+const firestore = firebase.firestore();
+
+const settings = { timestampsInSnapshots: true };
+firestore.settings(settings);
 
 // Add reactReduxFirebase enhancer when making store creator
 const createStoreWithFirebase = compose(
